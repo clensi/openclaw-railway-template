fd861831f72cfd772689c6aa059c9a0c92b13d60
clensi <50094053+clensi@users.noreply.github.com>
2026-05-06 18:45:55 +1000

    Create start-tailscale.sh for tailscale

---
+++ start-tailscale.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+/tailscale.d/tailscaled \
+  --state=/var/lib/tailscale/tailscaled.state \
+  --socket=/var/run/tailscale/tailscaled.sock \
+  --tun=userspace-networking \
+  --socks5-server=localhost:1055 \
+  --outbound-http-proxy-listen=localhost:1055 &
+
+# Wait for tailscaled to be ready
+until /tailscale.d/tailscale up \
+  --authkey="${TAILSCALE_AUTHKEY}" \
+  --hostname="${TAILSCALE_HOSTNAME}" \
+  ${TAILSCALE_ADDITIONAL_ARGS}
+do
+  sleep 0.2
+done
